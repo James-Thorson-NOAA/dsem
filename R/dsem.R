f291b30f3dc70ce9d2880021d8c06dc6456887df
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2026-05-09 10:00:05 -0700

    update docs

--- R/dsem.R
+++ R/dsem.R
@@ -157,6 +157,13 @@
 #' ecosystem dynamics constrained by ecological mechanisms.
 #' Methods in Ecology and Evolution. \doi{10.1111/2041-210X.14289}
 #'
+#' **Introducing moderated DSEM (e.g., nonstationarity and varying paths):**
+#'
+#' Thorson, J. T., Kristensen, K. (In press).
+#' Ecological examples of nonstationarity, nonlinearity, and
+#' statistical interactions in dynamic structural equation models.
+#' Methods in Ecology and Evolution. \url{https://ecoevorxiv.org/repository/view/11418/}
+#'
 #' @examples
 #' # Define model
 #' sem = "
